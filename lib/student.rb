3d913b4249c7de0b44bd6df858e7682d1b832b18
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-18 23:10:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,6 @@ require_relative "../config/environment.rb"
 
 class Student
   attr_accessor :name, :grade, :nil
-
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   def initialize(id=nilname, grade)
