0c1f2a8874d68e8f1e4f187d02465c6ce10637b7
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-18 23:07:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,7 +42,7 @@ class Student
 
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
-    DB[:conn].execute(sql, self.name, self.album, self.id)
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
   def self.create_table
